06ed96be6825a3f46f29be5a2be5765fa5bce5be
Emrecan Şuşter <emrecansuster@tuta.io>
2022-02-22 16:59:56 +0300

    some fixing and testing for confirmations model

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -4,6 +4,15 @@
 class ConfirmationsController < ApplicationController
   before_action :redirect_if_authenticated, only: %i[create new]
 
+  # @type [String]
+  OK_MESSAGE = 'Check the email for confirmations'
+  # @type [String]
+  CONFIRMED_MESSAGE = 'Confirmed!'
+  # @type [String]
+  INVALID_MESSAGE = 'Invalid token'
+  # @type [String]
+  NOT_AVAILABLE_MESSAGE = 'Confirmation is not available for this user'
+
   # @return [void]
   def create
     # @type [User]
@@ -11,9 +20,9 @@ class ConfirmationsController < ApplicationController
 
     if @user.present? && @user.unconfirmed?
       @user.send_confirmation_email!
-      redirect_to root_path, notice: 'Check the email for confirmations'
+      redirect_to root_path, notice: OK_MESSAGE
     else
-      redirect_to new_confirmation_path, alert: 'Confirmation is not available for this user'
+      redirect_to new_confirmation_path, alert: NOT_AVAILABLE_MESSAGE
     end
   end
 
@@ -25,10 +34,10 @@ class ConfirmationsController < ApplicationController
     if @user.present? && @user.unconfirmed_or_reconfirming?
       if @user.confirm!
         login @user
-        redirect_to root_path, notice: 'Confirmed!'
+        redirect_to root_path, notice: CONFIRMED_MESSAGE
       end
     else
-      redirect_to new_confirmation_path, alert: 'Invalid token'
+      redirect_to new_confirmation_path, alert: INVALID_MESSAGE
     end
   end
 
--- test/controllers/confirmations_controller_test.rb
+++ test/controllers/confirmations_controller_test.rb
@@ -1,7 +1,78 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ConfirmationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @unconfirmed_email = 'hey_this_is_my_first_account@rookie.com'
+    @unconfirmed_user = User.create!(
+      email: @unconfirmed_email,
+      password: 'Okay, Just give me the confirmation token',
+      password_confirmation: 'Okay, Just give me the confirmation token'
+    )
+
+    @confirmed_email = 'i_am_getting_bored_when_i_write_these_tests@bored.com'
+    @user = User.create!(
+      email: @confirmed_email,
+      password: '123456bored',
+      password_confirmation: '123456bored',
+      confirmed_at: Time.now
+    )
+  end
+
+  test 'If user is authenticated, the user should redirected except the confirm page' do
+    login @user
+
+    get new_confirmation_path
+    assert_redirected_to root_path
+  end
+
+  test 'If user is not authenticated, they can access the new confirmation page' do
+    get new_confirmation_path
+    assert_response :ok
+  end
+
+  test 'User is able to request confirmation token for unconfirmated users' do
+    assert_emails 1 do
+      post confirmations_path, params: {
+        user: { email: @unconfirmed_email }
+      }
+      assert_equal flash[:notice], ConfirmationsController::OK_MESSAGE
+    end
+  end
+
+  test 'User is not able to request confirmation token for confirmed user' do
+    assert_no_emails do
+      post confirmations_path, params: {
+        user: { email: @confirmed_email }
+      }
+      assert_equal flash[:alert], ConfirmationsController::NOT_AVAILABLE_MESSAGE
+    end
+  end
+
+  test 'User is not able to request confirmation for non-existing users' do
+    assert_no_emails do
+      post confirmations_path, params: {
+        user: { email: 'the_no_one@the_voidness_itself.com' }
+      }
+      assert_equal flash[:alert], ConfirmationsController::NOT_AVAILABLE_MESSAGE
+    end
+  end
+
+  test 'Anyone is able to confirm a user if they get to right token' do
+    token = @unconfirmed_user.generate_confirmation_token
+
+    get edit_confirmation_path(token)
+    assert_redirected_to root_path
+    assert_equal flash[:notice], ConfirmationsController::CONFIRMED_MESSAGE
+  end
+
+  test 'No one can confirm a user with an expired token' do
+    token = @unconfirmed_user.generate_confirmation_token
+
+    travel_to (User::CONFIRMATION_TOKEN_EXPIRATION + 1).from_now
+    get edit_confirmation_path(token)
+    assert_redirected_to new_confirmation_path
+    assert_equal flash[:alert], ConfirmationsController::INVALID_MESSAGE
+  end
 end
